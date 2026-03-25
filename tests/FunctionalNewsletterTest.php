c62602676094f59a318452a43303b69ec632ba05
BTS SIO <91031338+btssioslamlaval@users.noreply.github.com>
2026-03-25 22:49:18 +0100

    41

--- tests/FunctionalNewsletterTest.php
+++ tests/FunctionalNewsletterTest.php
@@ -3,8 +3,6 @@ use PHPUnit\Framework\TestCase;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\WebDriverBy;
-use Facebook\WebDriver\Chrome\ChromeOptions;
-use Facebook\WebDriver\WebDriverExpectedCondition;
 
 final class FunctionalNewsletterTest extends TestCase
 {
@@ -12,19 +10,10 @@ final class FunctionalNewsletterTest extends TestCase
 
     protected function setUp(): void
     {
-        $seleniumUrl = $_ENV['SELENIUM_URL'] ?? getenv('SELENIUM_URL') ?: 'http://localhost:4444';
-
-        $options = new ChromeOptions();
-        $options->addArguments([
-            '--headless',
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-        ]);
-
-        $capabilities = DesiredCapabilities::chrome();
-        $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
-
-        $this->driver = RemoteWebDriver::create($seleniumUrl, $capabilities);
+        $this->driver = RemoteWebDriver::create(
+            'http://localhost:4444',
+            DesiredCapabilities::chrome()
+        );
     }
 
     protected function tearDown(): void
@@ -36,19 +25,14 @@ final class FunctionalNewsletterTest extends TestCase
 
     public function testNewsletterAcceptsAdultWithValidEmail(): void
     {
-        $appUrl = $_ENV['APP_URL'] ?? getenv('APP_URL') ?: 'http://localhost:8000';
-        $this->driver->get($appUrl);
+        $this->driver->get("http://host.docker.internal:8000");
+
         $this->driver->findElement(WebDriverBy::id('age'))->sendKeys("20");
         $this->driver->findElement(WebDriverBy::id('email'))->sendKeys("test@test.com");
         $this->driver->findElement(WebDriverBy::id('btn'))->click();
 
-        $this->driver->wait(10, 500)->until(
-            WebDriverExpectedCondition::visibilityOfElementLocated(
-                WebDriverBy::id('result')
-            )
-        );
-
         $result = $this->driver->findElement(WebDriverBy::id('result'))->getText();
+
         $this->assertEquals("Inscription acceptée", $result);
     }
 }
