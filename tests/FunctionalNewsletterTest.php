4bdf504b229bb147b5832c7597280780553c5abb
BTS SIO <91031338+btssioslamlaval@users.noreply.github.com>
2026-03-25 17:51:50 +0100

    2eme essai avec Test

---
+++ tests/FunctionalNewsletterTest.php
@@ -0,0 +1,36 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
+
+final class FunctionalNewsletterTest extends TestCase
+{
+    private $driver;
+
+    protected function setUp(): void
+    {
+        $this->driver = RemoteWebDriver::create(
+            'http://localhost:4444/wd/hub',
+            DesiredCapabilities::chrome()
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        $this->driver->quit();
+    }
+
+    public function testNewsletterAcceptsAdultWithValidEmail()
+    {
+        $this->driver->get("http://localhost:8000");
+
+        $this->driver->findElement(WebDriverBy::id('age'))->sendKeys("20");
+        $this->driver->findElement(WebDriverBy::id('email'))->sendKeys("test@test.com");
+        $this->driver->findElement(WebDriverBy::id('btn'))->click();
+
+        $result = $this->driver->findElement(WebDriverBy::id('result'))->getText();
+
+        $this->assertEquals("Inscription acceptée", $result);
+    }
+}
